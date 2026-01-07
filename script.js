984f6c46d9025ea0046b938c0ba87caf041a7ea6
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-01-08 00:32:28 +0600

    Create solution for Problem 8: Capitalize first letter of each word using for...of loop, String & Array methods

--- script.js
+++ script.js
@@ -97,4 +97,20 @@ function findEvenNumbers(arr) {
     return evenNumbers;
 }
 
-console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));  
+// console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));  
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+    let words = str.split(" ");
+
+    let capitalizedWords = [];
+
+    for (let word of words) {
+        let capitalized = word[0].toUpperCase() + word.slice(1);
+        capitalizedWords.push(capitalized);
+    }
+
+    return capitalizedWords.join(" ");
+}
+
+// console.log(capitalizeWords("hello world")); 
