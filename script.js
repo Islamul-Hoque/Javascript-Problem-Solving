ed85e7e797dd71303050f8cd669e5ea3257e383b
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-01-07 23:22:30 +0600

    Add solution for Problem 2: Count vowels in a string using loop and conditionals

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ function reverseString(str) {
     return reversed;
 }
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i].toLowerCase();
+
+        if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+
+// console.log("2:", countVowels("programming"));
