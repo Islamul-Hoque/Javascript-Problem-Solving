cd450b65c4aeec698a26f0a62683fd9e11042111
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-01-07 23:28:49 +0600

    Implement Problem 4: Find maximum number in array using loop and comparison

--- script.js
+++ script.js
@@ -43,3 +43,16 @@ function isPalindrome(str) {
 
 // console.log("Problem-3A:", isPalindrome("madam"));
 // console.log("Problem-3B:", isPalindrome("hello"));
+
+// Problem 4: Find Maximum Number
+function findMax(arr) {
+    let max = arr[0]; 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([5, 1, 9, 3])); 
