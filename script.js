ded405a342073db5a8580abd74bade93bb7a5dc7
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-01-07 23:37:54 +0600

    Implement Problem 5: Remove duplicates from array using loop and includes

--- script.js
+++ script.js
@@ -55,4 +55,19 @@ function findMax(arr) {
     return max;
 }
 
-console.log(findMax([5, 1, 9, 3])); 
+// console.log(findMax([5, 1, 9, 3])); 
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+    let unique = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!unique.includes(arr[i])) {
+            unique.push(arr[i]);
+        }
+    }
+
+    return unique;
+}
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
